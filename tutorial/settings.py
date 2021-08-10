4d64a62c4b8275a839c00f1809297f9c803f3ddf
renteadan <62014192+renteadan@users.noreply.github.com>
2021-08-10 12:27:12 +0300

    rest api test + mysqldb

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets.apps.SnippetsConfig'
 ]
 
 MIDDLEWARE = [
@@ -82,8 +81,10 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str(BASE_DIR / '.env'),
+        }
     }
 }
 
