723f97f88c1615943b688912ee2e35b0ec068300
renteadan <62014192+renteadan@users.noreply.github.com>
2021-08-18 15:43:40 +0300

    update m-m relation

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
 
     class Meta:
         model = Item
@@ -11,6 +12,7 @@ class ItemSerializer(serializers.ModelSerializer):
 
 class ShoppingListItemSerializer(serializers.ModelSerializer):
     item = ItemSerializer()
+    id = serializers.IntegerField(required=False)
 
     class Meta:
         model = ShoppingListItem
@@ -24,3 +26,29 @@ class ShoppingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingList
         fields = ['id', 'name', 'items']
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.items.set([])
+        instance.save()
+        items = validated_data.get('shoppinglistitem_set')
+        for item in items:
+            item_id = item.get('id')
+            print(item_id)
+            if item_id:
+                item_obj = ShoppingListItem.objects.get(id=item_id)
+                item_obj.quantity = item.get('quantity')
+                item_obj.save()
+            else:
+                base_item_id = item.get('item').get('id')
+                if base_item_id:
+                    base_item = Item.objects.get(id=base_item_id)
+                    ShoppingListItem.objects.create(
+                        list=instance,
+                        item=base_item,
+                        quantity=item.get('quantity')
+                    )
+                else:
+                    raise serializers.ValidationError(
+                        'Item id is required.')
+        return instance
